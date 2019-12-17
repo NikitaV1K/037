e462f48097dbbb5441b39cec7944287507aa2aa8
b302-08 <b302-08@gmail.com>
2019-12-17 11:51:29 +0300

    first commit

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Form2 form2 = new Form2();
+            form2.Show();
+            this.Hide();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if ((textBox1.Text == "login") && (textBox2.Text == "pass"))
+
+            {
+                Form3 form3 = new Form3();
+                form3.Show();
+                this.Hide();
+
+            }
+
+            else MessageBox.Show("Введен не верный логин или пароль");
+        }
+    }
+}
+
